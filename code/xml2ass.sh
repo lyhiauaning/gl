a30a13641ba80c8eb37f87d83a6f2dbf4e80bd81
lyhiauaning <lyhiauaning@163.com>
2021-04-17 00:34:19 +0800

    支持xml2ass *

--- code/xml2ass.sh
+++ code/xml2ass.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-if [[ "$1" == "--all" ]];then
+if [[ "$1" == "--all" || "$1" == "-a" ]];then
   ls $2/*.xml | xargs -n 1 -d '\n' xml2ass
 else
-  if expr "$1" : ".*\.cmt\.xml" &> /dev/null ;then
-    python $PREFIX/etc/gl/danmaku2ass.py -o "${1%.cmt.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$1"
-  elif expr "$1" : ".*\.xml" &> /dev/null ;then
-    python $PREFIX/etc/gl/danmaku2ass.py -o "${1%.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$1"
-  fi
+  for i in $@
+  do
+    if expr "$i" : ".*\.cmt\.xml" &> /dev/null ;then
+      python $PREFIX/etc/gl/danmaku2ass.py -o "${i%.cmt.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$i"
+    elif expr "$i" : ".*\.xml" &> /dev/null ;then
+      python $PREFIX/etc/gl/danmaku2ass.py -o "${i%.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$i"
+    fi
+  done
 fi

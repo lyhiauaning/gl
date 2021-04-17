4caa5d54386d669ac25cc031ab201b521d75466d
lyhiauaning <lyhiauaning@163.com>
2021-04-17 11:37:29 +0800

    支持xml2ass自动转换当前目录所有xml弹幕及更新README.md

--- code/xml2ass.sh
+++ code/xml2ass.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-if [[ "$1" == "--all" || "$1" == "-a" ]];then
-  ls $2/*.xml | xargs -n 1 -d '\n' xml2ass
+if [[ "$#" > "1" ]];then
+  if [[ "$1" == "--all" || "$1" == "-a" ]];then
+    ls $2/*.xml | xargs -n 1 -d '\n' xml2ass
+  else
+    for i in $@
+    do
+      if expr "$i" : ".*\.cmt\.xml" &> /dev/null ;then
+        python $PREFIX/etc/gl/danmaku2ass.py -o "${i%.cmt.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$i"
+      elif expr "$i" : ".*\.xml" &> /dev/null ;then
+        python $PREFIX/etc/gl/danmaku2ass.py -o "${i%.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$i"
+      fi
+    done
+  fi
 else
-  for i in $@
-  do
-    if expr "$i" : ".*\.cmt\.xml" &> /dev/null ;then
-      python $PREFIX/etc/gl/danmaku2ass.py -o "${i%.cmt.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$i"
-    elif expr "$i" : ".*\.xml" &> /dev/null ;then
-      python $PREFIX/etc/gl/danmaku2ass.py -o "${i%.xml*}.ass" -s 1920x1080 -fn "Microsoft Yahei" -fs 48 -a 0.8 -dm 14 -ds 6 "$i"
-    fi
-  done
+  ls ./*.xml | xargs -n 1 -d '\n' xml2ass
 fi
